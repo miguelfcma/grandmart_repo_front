31587273895a395e699e88be4cf5f46891fc2fbb
miguelfcma <107379032+miguelfcma@users.noreply.github.com>
2023-03-10 21:04:29 -0600

    ramita

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 
   appId: "1:589248633150:web:170209812c5aaffc2ef46f",
 };
-
+//ESTE ES UN CAMBIO DE RAMITA
 //brayan es puto
 //ESTE ES EL NUEVO SECSO EN EL OXXO
 const app = initializeApp(firebaseConfig);
