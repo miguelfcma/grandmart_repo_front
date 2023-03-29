91020ac1c3bf450f224c1f2b64f7bd8d7cf249e3
brayancg <brayanchavez179@gmail.com>
2023-03-29 16:07:44 -0600

    Versión 4:07 pm 29/mar

--- src/components/ProductoComponents/ListProductsGeneral.jsx
+++ src/components/ProductoComponents/ListProductsGeneral.jsx
@@ -1,29 +1,58 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { CardProducto } from "./client/CardProducto";
 import "./ListProductsGeneral.css";
 import { useProductos } from "./ProductosContext/ProductoProvider";
 
 export function ListProductsGeneral() {
   const { productos, loadProductos } = useProductos();
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 9;
 
   useEffect(() => {
     loadProductos();
   }, []);
 
   function renderMain() {
-    if (productos.length === 0) {
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = productos.slice(
+      indexOfFirstProduct,
+      indexOfLastProduct
+    );
+
+    if (currentProducts.length === 0) {
       return <h1>No hay productos registrados</h1>;
     } else {
-      return productos.map((producto) => (
+      return currentProducts.map((producto) => (
         <CardProducto key={producto.id} producto={producto} />
       ));
     }
+  }
+
+  function renderPagination() {
+    const totalPages = Math.ceil(productos.length / productsPerPage);
+    const pages = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(
+        <a
+          key={i}
+          className={i === currentPage ? "active" : ""}
+          onClick={() => setCurrentPage(i)}
+        >
+          {i}
+        </a>
+      );
+    }
+
+    return <div className="pagination">{pages}</div>;
   }
 
   return (
     <>
       <h2 className="titulo">Lista de productos:</h2>
       <div className="list-productos">{renderMain()}</div>
+      {renderPagination()}
     </>
   );
 }
