a019d98f9e0cbbc99d9e92bbd694a72c7cdb8e87
miguelfcma <107379032+miguelfcma@users.noreply.github.com>
2023-04-01 08:16:04 -0600

    1 de abril 8:09 pm

--- src/pages/DashAdminPages/DashPages/PerfilPage.jsx
+++ src/pages/DashAdminPages/DashPages/PerfilPage.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { Sidebar } from "../../../components/DashAdminComponents/Sidebar";
 import { CardUsuarioDomicilio } from "../../../components/usuarioComponents/UsuarioPerfilComponents/CardUsuarioDomicilio";
 export function PerfilPage() {
@@ -7,17 +6,6 @@ export function PerfilPage() {
       <Sidebar />
       
       <CardUsuarioDomicilio />
-=======
-import { DashAdmin } from "../DashAdmin";
-import { FormUsuarioDomicilio } from "../../../components/usuarioComponents/UsuarioPerfilComponents/FormUsuarioDomicilio";
-import { CardUsuarioDomicilio } from "../../../components/usuarioComponents/UsuarioPerfilComponents/CardUsuarioDomicilio";
-export  function PerfilPage() {
-  return (
-    <div style={{ marginLeft: '200px' }}>
-      <DashAdmin />
-        <FormUsuarioDomicilio/>
-      <CardUsuarioDomicilio/>
->>>>>>> Stashed changes
     </div>
   );
 }
--- src/pages/HomePages/Productos-Pages/ProductosDetallesPage.jsx
+++ src/pages/HomePages/Productos-Pages/ProductosDetallesPage.jsx
@@ -3,42 +3,7 @@ import { DetallesProducto } from "../../../components/ProductoComponents/client/
 import { Navbar } from "../../../components/HomePageComponents/NavBar";
 
 export function ProductosDetallesPage() {
-<<<<<<< Updated upstream
   const { id } = useParams();
-=======
-  const { productos, getImgPortadaProducto, getProductImagesGaleria, loadProductos } = useProductos();
-  const { id } = useParams();
-
-  const [producto, setProducto] = useState(null);
-  const [imagen, setImagen] = useState(null);
-  const [imagenes, setImagenes] = useState(null);
-  const { categorias, loadCategorias } = useCategorias();
-
-  useEffect(() => {
-    loadCategorias();
-    loadProductos();
-  }, []);
-
-  useEffect(() => {
-    const productoEncontrado = productos.find(
-      (prod) => prod.id === parseInt(id)
-    );
-
-    if (productoEncontrado) {
-      setProducto(productoEncontrado);
-
-      async function cargarImagen() {
-        const urlImagen = await getImgPortadaProducto(parseInt(id));
-        const imagenesArray = await getProductImagesGaleria(parseInt(id));
-        setImagenes(imagenesArray);
-        setImagen(urlImagen);
-      }
-
-      cargarImagen();
-    }
-  }, [productos, id, getImgPortadaProducto, getProductImagesGaleria]);
-
->>>>>>> Stashed changes
   return (
     <>
        <Navbar/>
