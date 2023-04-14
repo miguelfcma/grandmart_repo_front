a901a11af4bdeedbe968f325d2ad33327fed7ca9
brayancg <brayanchavez179@gmail.com>
2023-04-13 20:26:37 -0600

    version 8:26pm 13/abr

--- src/components/ProductoComponents/ProductosGeneral/VistaDetallesProductoGeneral/DetallesProductoGeneral.jsx
+++ src/components/ProductoComponents/ProductosGeneral/VistaDetallesProductoGeneral/DetallesProductoGeneral.jsx
@@ -2,10 +2,9 @@ import { useProductos } from "../../ProductosContext/ProductoProvider";
 import { useEffect, useState } from "react";
 import { ComentariosProducto } from "../ComentariosProductoGeneral/ComentariosProducto";
 import { ReviewsProducto } from "../ReviewsProductosGeneral/ReviewsProducto";
-import styles from "./DetallesProductoGeneral.module.css";
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-import Button from 'react-bootstrap/Button';
+import "./DetallesProductoGeneral.css";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
 
 export function DetallesProductoGeneral({ id }) {
   const {
@@ -42,7 +41,7 @@ export function DetallesProductoGeneral({ id }) {
     }
   }, [productosAll, id, getImgPortadaProducto, getProductImagesGaleria]);
 
-  const [zoom, setZoom] = useState(1);
+  const [setZoom] = useState(1);
   const handleZoomIn = () => {
     setZoom(1.5);
   };
@@ -52,60 +51,77 @@ export function DetallesProductoGeneral({ id }) {
 
   const [imagenZoom, setImagenZoom] = useState(null);
 
-
   return (
-    <div className={styles.container}>
-
-        <div className={styles.galeria}>
-          <div className={styles.imagenAmpliada}
-            style={{ display: imagenZoom ? "block" : "none" }}>
-            <img src={imagenZoom} alt="Imagen ampliada" />
+    <div className="contenedor">
+    <div>
+      {producto && (
+        <>
+          <div className="infoProductoTitulo">
+            <Card.Title style={{ fontSize: "25px" }}>
+              {producto.nombre}
+            </Card.Title>
           </div>
+          <br></br>
+          <div className="galeria">
+          <div
+              className="imagenAmpliada"
+              style={{ display: imagenZoom ? "block" : "none" }}
+            >
+              <img src={imagenZoom} alt="Imagen ampliada" />
+            </div>
             <CardGroup>
               {imagenPortada && (
                 <Card>
-                    <Card.Img variant="top" src={imagenPortada} alt="Portada"
-                      className={imagenPortada === imagenZoom ? styles.zoom : ""}
-                      onMouseEnter={() => setImagenZoom(imagenPortada)}
-                      onMouseLeave={() => setImagenZoom(null)}
-                    />
+                  <Card.Img
+                    variant="top"
+                    src={imagenPortada}
+                    alt="Portada"
+                    className={imagenPortada === imagenZoom ? "zoom" : ""}
+                    onMouseEnter={() => setImagenZoom(imagenPortada)}
+                    onMouseLeave={() => setImagenZoom(null)}
+                  />
                 </Card>
               )}
               {imagenes &&
                 imagenes.map((imagen) => (
                   <Card key={imagen.id}>
-                      <Card.Img variant="top" src={imagen.url} alt={imagen.id}
-                        className={imagen.url === imagenZoom ? styles.zoom : ""}
-                        onMouseEnter={() => setImagenZoom(imagen.url)}
-                        onMouseLeave={() => setImagenZoom(null)}
-                      />
+                    <Card.Img
+                      variant="top"
+                      src={imagen.url}
+                      alt={imagen.id}
+                      className={imagen.url === imagenZoom ? "zoom" : ""}
+                      onMouseEnter={() => setImagenZoom(imagen.url)}
+                      onMouseLeave={() => setImagenZoom(null)}
+                    />
                   </Card>
                 ))}
             </CardGroup>
-        </div>
+          </div>
 
-        <br></br>
+          <div className="infoProducto">
+            <Card
+              style={{
+                width: "98%",
+                margin: "0 auto",
+                padding: "15px 20px 15px 20px",
+              }}
+            >
+              <Card.Title>
+                <div className="infoProductoPrecio">${producto.precio}</div>
+                <br></br>
 
-      <div className={styles.infoProducto}>
-        {producto ? (
-          <>
-          <Card style={{ width: '98%', margin: '0 auto' }}>
-            <Card.Title>
-            <div className={styles.infoProductoTitulo}>{producto.nombre}</div>
-            <div className={styles.infoProductoPrecio}>${producto.precio}</div>
-            
-            <div className={styles.infoProductoDescripcion}>
-              {producto.descripcion}
-            </div>
-            <div className={styles.infoProductoCaracteristicas}>
-              <div>Marca: {producto.marca}</div>
-              <div>Modelo: {producto.modelo}</div>
-              <div>Color: {producto.color}</div>
-              <div>Estado: {producto.estado ? "Nuevo" : "Usado"}</div>
-              <div>Categoría: {producto.categoria.nombre}</div>
-              <div>Producto públicado por: {producto.usuario.nombre}</div>
-            </div>
-            </Card.Title>
+                <div className="infoProductoDescripcion">{producto.descripcion}</div>
+                <br></br>
+
+                <div className="infoProductoCaracteristicas">
+                  <div>Marca: {producto.marca}</div>
+                  <div>Modelo: {producto.modelo}</div>
+                  <div>Color: {producto.color}</div>
+                  <div>Estado: {producto.estado ? "Nuevo" : "Usado"}</div>
+                  <div>Categoría: {producto.categoria.nombre}</div>
+                  <div>Producto públicado por: {producto.usuario.nombre}</div>
+                </div>
+              </Card.Title>
             </Card>
 
             <br></br>
@@ -113,11 +129,10 @@ export function DetallesProductoGeneral({ id }) {
             <button onClick={() => window.history.back()}>Regresar</button>
             <ComentariosProducto />
             <ReviewsProducto />
-          </>
-        ) : (
-          <div>No se encontró el producto</div>
-        )}
-      </div>
+          </div>
+        </>
+      )}
+    </div>
     </div>
   );
 }
