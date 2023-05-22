e13aea9deb7a545bf192516b5591b1b4a58fec43
brayancg <brayanchavez179@gmail.com>
2023-05-22 04:11:05 -0600

    version 4:11am 22 de mayo

--- src/components/ServicioComponents/ServiciosGeneral/ListaGeneralServicios/VistaDetallesServicioGeneral/DetallesServicioGeneral.jsx
+++ src/components/ServicioComponents/ServiciosGeneral/ListaGeneralServicios/VistaDetallesServicioGeneral/DetallesServicioGeneral.jsx
@@ -1,5 +1,6 @@
 import { useServicios } from "../../../ServiciosContext/ServicioProvider";
 import { useEffect, useState } from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
 /* import { ReviewsServicio } from "../ReviewsServicioGeneral/ReviewsServicio"; 
 
@@ -8,8 +9,6 @@ import { PreguntasServicioComponenteCompletoGeneral } from "../PreguntasServicio
 */
 
 import "./DetallesServicioGeneral.css";
-import Card from "react-bootstrap/Card";
-import CardGroup from "react-bootstrap/CardGroup";
 
 export function DetallesServicioGeneral({ id, nombre_categoria, nombre_usuario }) {
   const {
@@ -22,6 +21,7 @@ export function DetallesServicioGeneral({ id, nombre_categoria, nombre_usuario }
   const [servicio, setServicio] = useState(null);
   const [imagenPortada, setImagenPortada] = useState(null);
   const [imagenes, setImagenes] = useState(null);
+  const [imagenSeleccionada, setImagenSeleccionada] = useState(null);
 
   useEffect(() => {
     loadServicios();
@@ -46,94 +46,140 @@ export function DetallesServicioGeneral({ id, nombre_categoria, nombre_usuario }
     }
   }, [serviciosAll, id, getImgPortadaServicio, getAllImagesServicio]);
 
-  const [setZoom] = useState(1);
-  const handleZoomIn = () => {
-    setZoom(1.5);
+
+
+  const handleImagenHover = (url) => {
+    setImagenSeleccionada(url);
   };
-  const handleZoomOut = () => {
-    setZoom(1);
+
+  const [zoomStyle, setZoomStyle] = useState({ visibility: "hidden" });
+
+  const handleMouseMove = (e) => {
+    const { left, top, width, height } =
+      e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - left;
+    const y = e.clientY - top;
+    const zoomX = ((x / width) * 100).toFixed(2);
+    const zoomY = ((y / height) * 100).toFixed(2);
+    setZoomStyle({
+      visibility: "visible",
+      transform: `translate(-${zoomX}%, -${zoomY}%) scale(3)`,
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setZoomStyle({ visibility: "hidden" });
   };
 
   const [imagenZoom, setImagenZoom] = useState(null);
 
   return (
-    <div className="contenedor">
-    <div>
-      {servicio && (
-        <>
-          <div className="infoProductoTitulo">
-            <Card.Title style={{ fontSize: "25px" }}>
-              {servicio.titulo}
-            </Card.Title>
+    <>
+      <Container fluid className="contenedor">
+        <Col xs={12} md={5} lg={4} className="columna-izquierda">
+          <br />
+          <br />
+          <br />
+          <br />
+          <div className="imagen-container">
+            {imagenPortada && (
+              <img
+                src={imagenPortada}
+                alt="Portada"
+                className="imagen-producto"
+                onMouseOver={() => handleImagenHover(imagenPortada)}
+              />
+            )}
+            {imagenes &&
+              imagenes.map((imagen) => (
+                <img
+                  key={imagen.id}
+                  src={imagen.url}
+                  alt={imagen.id}
+                  className="imagen-producto"
+                  onMouseOver={() => handleImagenHover(imagen.url)}
+                />
+              ))}
           </div>
-          <br></br>
-          <div className="galeria">
-          <div
-              className="imagenAmpliada"
-              style={{ display: imagenZoom ? "block" : "none" }}
-            >
-              <img src={imagenZoom} alt="Imagen ampliada" />
-            </div>
-            <CardGroup>
-              {imagenPortada && (
-                <Card>
-                  <Card.Img
-                    variant="top"
-                    src={imagenPortada}
-                    alt="Portada"
-                    className={imagenPortada === imagenZoom ? "zoom" : ""}
-                    onMouseEnter={() => setImagenZoom(imagenPortada)}
-                    onMouseLeave={() => setImagenZoom(null)}
+        </Col>
+        <Col xs={12} md={7} lg={8} className="columna-central">
+          <br />
+          <br />
+          <br />
+          <br />
+  
+          <Row>
+            {imagenSeleccionada === null ? (
+              <div className="imagen-central-container">
+                <img
+                  src={imagenPortada}
+                  alt="Portada"
+                  className="imagen-central"
+                />
+              </div>
+            ) : (
+              <div
+                className="imagen-central-container"
+                onMouseMove={(e) => handleMouseMove(e)}
+                onMouseLeave={() => handleMouseLeave()}
+              >
+                <img
+                  src={imagenSeleccionada}
+                  alt="Seleccionada"
+                  className="imagen-central"
+                />
+                <div className="zoom-container" style={zoomStyle}>
+                  <img
+                    src={imagenSeleccionada}
+                    alt="Zoom"
+                    className="zoom-img"
+                    style={{ width: "100%", height: "100%" }}
                   />
-                </Card>
-              )}
-              {imagenes &&
-                imagenes.map((imagen) => (
-                  <Card key={imagen.id}>
-                    <Card.Img
-                      variant="top"
-                      src={imagen.url}
-                      alt={imagen.id}
-                      className={imagen.url === imagenZoom ? "zoom" : ""}
-                      onMouseEnter={() => setImagenZoom(imagen.url)}
-                      onMouseLeave={() => setImagenZoom(null)}
-                    />
-                  </Card>
-                ))}
-            </CardGroup>
-          </div>
-
-          <div className="infoProducto">
-            <Card
-              style={{
-                width: "98%",
-                margin: "0 auto",
-                padding: "15px 20px 15px 20px",
-              }}
-            >
-              <Card.Title>
-                <div className="infoProductoPrecio">${servicio.precio}</div>
-                <br></br>
-
-                <div className="infoProductoDescripcion">{servicio.descripcion}</div>
-                <br></br>
-
-                <div className="infoProductoCaracteristicas">
-                  <div>Categoría: {servicio.categoria.nombre}</div>
-                  <div>Servicio publicado por: {servicio.usuario.nombre}</div>
                 </div>
-              </Card.Title>
-            </Card>
-
-            <br></br>
-
-            <button onClick={() => window.history.back()}>Regresar</button>
-            
-          </div>
-        </>
-        
-      )}
-    </div>
-    </div>
+              </div>
+            )}
+          </Row>
+        </Col>
+  
+        <Col xs={8} lg={12} className="columna-derecha">
+          <br />
+          <br />
+          <br />
+          <br />
+          {servicio && (
+            <Col>
+              <Card className="infoProducto">
+                <div className="d-flex justify-content-end position-absolute top-0 end-0 p-2"></div>
+  
+                <Card.Title>
+                  <div className="infoProductoTitulo">{servicio.nombre}</div>
+                </Card.Title>
+                <Card.Title>
+                  <div className="infoProductoPrecio">${servicio.precio}</div>
+                  <br />
+  
+                  <div className="infoProductoDescripcion">{servicio.descripcion}</div>
+                  <br />
+  
+                  <div className="infoProductoCaracteristicas">
+                    <div>Categoría: {servicio.categoria.nombre}</div>
+                    <div>Servicio publicado por: {servicio.usuario.nombre}</div>
+                  </div>
+                </Card.Title>
+              </Card>
+  
+              <br />
+  
+              <Button
+                onClick={() => window.history.back()}
+                className="back-button3"
+              >
+                Atrás
+              </Button>
+            </Col>
+          )}
+        </Col>
+      </Container>
+    </>
   );
-}
+}  
