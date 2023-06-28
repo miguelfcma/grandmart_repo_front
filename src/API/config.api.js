800c1ea14281aaf6b50e9de8e6b9cf0e7cd8b63e
miguelfcma <107379032+miguelfcma@users.noreply.github.com>
2023-06-27 22:28:41 -0600

    firebase config api 1.1

--- src/API/config.api.js
+++ src/API/config.api.js
@@ -1,4 +1,4 @@
 //export const API_BASE_URL = "http://localhost:4000/"
 //export const API_BASE_URL = "https://b114-2806-104e-1a-1e84-89e-6179-59ff-25c8.ngrok-free.app/"
 
-export const API_BASE_URL = "ec2-44-202-59-151.compute-1.amazonaws.com:4000/"
+export const API_BASE_URL = "http://ec2-44-202-59-151.compute-1.amazonaws.com:4000/";
