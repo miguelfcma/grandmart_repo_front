070e683e08906a401c3aa611effc6988f83e2bd0
brayancg <brayanchavez179@gmail.com>
2023-09-26 18:38:19 -0600

    primer commit

--- src/components/ProductoComponents/ProductosGeneral/ListaGeneralProductosFitrado/FiltradoProductosPorBusqueda.jsx
+++ src/components/ProductoComponents/ProductosGeneral/ListaGeneralProductosFitrado/FiltradoProductosPorBusqueda.jsx
@@ -5,13 +5,9 @@ import { useProductos } from "../../ProductosContext/ProductoProvider";
 import { useServicios } from "../../../ServicioComponents/ServiciosContext/ServicioProvider";
 
 export function FiltradoProductosPorBusqueda({ searchTerm }) {
-  const { productosAll, loadProductos, favoritos, loadFavoritos } =
-    useProductos();
+  const { productosAll, loadProductos, favoritos, loadFavoritos } = useProductos();
   const { serviciosAll, loadServicios } = useServicios();
   const usuario = JSON.parse(localStorage.getItem("usuario"));
-
-  // Declarar la variable noHayResultados fuera de la función renderMain()
-  const noHayResultados = `No hay publicaciones que coincidan con tu búsqueda "${searchTerm}".`;
 
   useEffect(() => {
     loadProductos();
@@ -24,95 +20,65 @@ export function FiltradoProductosPorBusqueda({ searchTerm }) {
     }
   }, []);
 
-  function renderMain() {
-    let filteredProducts = productosAll;
-    let filteredServicios = serviciosAll;
-
-    // Filtrar los productos basados en el searchTerm
+  function renderProductos() {
     const filteredProductos = productosAll.filter(
       (producto) =>
         producto.nombre.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        producto.categoria.nombre
-          .toLowerCase()
-          .includes(searchTerm.toLowerCase()) ||
+        producto.categoria.nombre.toLowerCase().includes(searchTerm.toLowerCase()) ||
         producto.descripcion.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
-    // Filtrar los servicios basados en el searchTerm
-    const filterServicios = serviciosAll.filter(
+    return (
+      <div>
+        {filteredProductos.map((producto) => (
+          <div key={producto.id}>
+            <CardProductoGeneral producto={producto} favoritos={favoritos} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  function renderServicios() {
+    const filteredServicios = serviciosAll.filter(
       (servicio) =>
         servicio.titulo.toLowerCase().includes(searchTerm.toLowerCase()) ||
         servicio.descripcion.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
-    if (filteredProductos.length === 0 || filteredServicios.length === 0) {
-      return (
-        <div>
-          <div style={{ paddingTop: "30px", fontSize: "30px" }}>
-            {noHayResultados}
-            <br></br> <br></br>* Revisa la ortografía de la palabra. <br></br>*
-            Utiliza palabras más genéricas o menos palabras. <br></br>* Navega
-            por las categorías para encontrar un producto similar.
+    return (
+      <div>
+        {filteredServicios.map((servicio) => (
+          <div key={servicio.id}>
+            <CardServicioGeneral servicio={servicio} />
           </div>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-        </div>
-      );
-    } else {
-      const siHayResultados = `Resultados para "${searchTerm}".`;
-      return (
-        <div>
-          <div style={{ fontSize: "30px", paddingTop: "5px", marginLeft: "35px"}}>
-            {siHayResultados}
-          </div>
-          <div style={{ display: "flex", flexWrap: "wrap", marginLeft: "35px"}}>
-            {filteredProductos.map((producto) => (
-              <div
-                key={producto.id}
-                style={{
-                  marginTop: "15px",
-                  marginRight: "35px",
-                  display: "flex",
-                  flexWrap: "wrap",
-                  justifyContent: "space-between",
-                  gap: "20px",
-                  alignItems: "stretch",
-                }}
-              >
-                <CardProductoGeneral
-                  key={producto.id}
-                  producto={producto}
-                  favoritos={favoritos}
-                />
-              </div>
-            ))}
-
-            {filterServicios.map((servicio) => (
-              <div
-                key={servicio.id}
-                style={{
-                  marginTop: "15px",
-                  marginRight: "35px",
-                  display: "flex",
-                  flexWrap: "wrap",
-                  justifyContent: "space-between",
-                  gap: "20px",
-                  alignItems: "stretch",
-                }}
-              >
-                <CardServicioGeneral servicio={servicio} />
-              </div>
-            ))}
-          </div>
-        </div>
-      );
-    }
+        ))}
+      </div>
+    );
   }
+
+  const hayProductos = productosAll.length > 0 && renderProductos().props.children.length > 0;
+  const hayServicios = serviciosAll.length > 0 && renderServicios().props.children.length > 0;
+
   return (
-    <>
-      <div className="filtProductos">{renderMain()}</div>
-    </>
+    <div>
+      {hayProductos && (
+        <div style={{ fontSize: "30px", paddingTop: "5px", marginLeft: "35px" }}>
+          Resultados de productos para "{searchTerm}".
+        </div>
+      )}
+      <div style={{ display: "flex", flexWrap: "wrap", marginLeft: "35px" }}>
+        {hayProductos && renderProductos()}
+      </div>
+
+      {hayServicios && (
+        <div style={{ fontSize: "30px", paddingTop: "5px", marginLeft: "35px" }}>
+          Resultados de servicios para "{searchTerm}".
+        </div>
+      )}
+      <div style={{ display: "flex", flexWrap: "wrap", marginLeft: "35px" }}>
+        {hayServicios && renderServicios()}
+      </div>
+    </div>
   );
 }
